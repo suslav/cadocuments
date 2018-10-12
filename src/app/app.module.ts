31b57d68b8702f732c58dc1947cde4f176e53270
suslavkrishna@gmail.com <suslavkrishna@gmail.com>
2018-10-12 14:39:04 +0530

    add the new implement

---
+++ src/app/app.module.ts
@@ -0,0 +1,61 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatGridListModule,
+  MatMenuModule,
+  MatIconModule,
+  MatToolbarModule
+} from "@angular/material"
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { AngularFireModule, } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database';
+var fireAppconfig = {
+  apiKey: "AIzaSyDqnNcj9H45NVXd3Rw4pGXRQYfe2czCxaU",
+  authDomain: "document-34e6c.firebaseapp.com",
+  databaseURL: "https://document-34e6c.firebaseio.com",
+  projectId: "document-34e6c",
+  storageBucket: "document-34e6c.appspot.com",
+  messagingSenderId: "541646341268"
+};
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([]),
+    FlexLayoutModule,
+    AngularFireModule.initializeApp(fireAppconfig),
+    AngularFireDatabaseModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
