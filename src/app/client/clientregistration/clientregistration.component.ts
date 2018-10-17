be5814a88652ec4e50133d35322d99732fd260b5
mahesh.k-v <maheshvrsecit@gmail.com>
2018-10-17 10:33:30 +0530

    add bug

--- src/app/client/clientregistration/clientregistration.component.ts
+++ src/app/client/clientregistration/clientregistration.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Clients } from 'src/app/models/userlogin';
-import { AuthService } from '../../services/auth.service';
+
 import{FormBuilder, Validators, ControlContainer, FormGroup, FormControl} from '@angular/forms';
+import { DocumentService } from 'src/app/services/document.service';
 
 @Component({
   selector: 'app-clientregistration',
@@ -10,13 +11,13 @@ import{FormBuilder, Validators, ControlContainer, FormGroup, FormControl} from '
 })
 export class ClientregistrationComponent implements OnInit {
   public clientRegistration: Clients;
-  constructor(private authservice:AuthService) { }
+  constructor(private documentserivice:DocumentService) { }
 
   ngOnInit() {
     this.loadClientRegist();
   }
   clientRegister(clientRegistration:Clients,isValid:boolean){
-      this.authservice.clientRegister(clientRegistration).then(res=>{
+      this.documentserivice.clientRegister(clientRegistration).then(res=>{
         console.log(res);
       },error=>{
         console.log(error)
